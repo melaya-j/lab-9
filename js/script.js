18b09ba8b282fc7d37b5897c9f5b70627b5508fc
melaya-j <melaya710@gmail.com>
2026-04-08 20:38:38 -0500

    var for text box

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 function init(){
   var button = document.getElementById('entrybutton');
+
   function showMeText(){
+    var textbox = document.getElementById('entryinput');
   }
 
   button.addEventListener('click', showMeText);
