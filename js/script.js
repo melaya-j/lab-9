0db0a39a76707d1b68918bc7019e55cc431691d4
melaya-j <melaya710@gmail.com>
2026-04-08 20:39:28 -0500

    alert my name

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function init(){
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Melaya Thomas: " + textbox.value);
   }
 
   button.addEventListener('click', showMeText);
